87518554528a13c2d076db22d4accc86bb970587
Manuel <licealabradamanuel@gmail.com>
2026-03-14 21:16:23 -0600

    actualizacion del diseño

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function mostrar(seccion){
 
-let secciones = document.querySelectorAll("section");
+let secciones=document.querySelectorAll("section");
 
 secciones.forEach(sec=>{
 sec.classList.add("oculto");
@@ -8,4 +8,22 @@ sec.classList.add("oculto");
 
 document.getElementById(seccion).classList.remove("oculto");
 
+if(window.innerWidth<900){
+document.querySelector(".menu").classList.remove("activo");
 }
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+const boton=document.createElement("div");
+boton.innerHTML="☰";
+boton.classList.add("hamburguesa");
+
+document.body.appendChild(boton);
+
+boton.addEventListener("click",()=>{
+document.querySelector(".menu").classList.toggle("activo");
+});
+
+});
